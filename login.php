7909ff0361c054b185545f0434fb92cfb53b727f
atm0016 <atm0016@example.com>
2017-05-29 16:17:58 +0530

    Added function of session

--- login.php
+++ login.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+// setting to conect db and php
+$severname = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($severname,$username,$password);
+$db = mysqli_select_db($conn,"misao_small_demo");
+
 // 配列の中身を明示してくれる
 echo '<pre>';
 var_dump($_POST);
@@ -20,6 +28,29 @@ if(isset($_POST['login']) && $_POST['login'] === "Login"){
   }
   elseif (! preg_match("/^[a-z A-Z 0-9]+$/",$password)) {
     $errors['password2'] = "Enter a valid password";
+  }
+  else {
+    // email and password sent from form
+      $array = "select * from user_information where email='".$email."' and password = '".$password."'";
+      $result = mysqli_query($conn,$array);
+      $count = mysqli_num_rows($result);
+      $data = mysqli_fetch_array($result);
+    // If result matched $email and $password, table row must be 1 row
+      if($count >= 1) {
+        $_SESSION['login_user'] = $data["name"];
+        header("location: welcome.php");
+      }else {
+        echo "Your Login Name or Password is invalid";
+        //  $error = "Your Login Name or Password is invalid";
+      }
+    // 確認---------------------
+        // if($run){
+        //   echo "success";
+        // }
+        // else {
+        //   echo "error.";
+        // }
+    // ------------------------------------
   }
 }
 ?>
--- welcome.php
+++ welcome.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if (! isset($_SESSION["login_user"])) {
+  header("location: login.php");
+}
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,5 +12,6 @@
   </head>
   <body>
     <h1>WELCOME!!!</h1>
+    <a href="index.php">HOME</a>
   </body>
 </html>
