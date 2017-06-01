aa2df102d764edbde6a13cd9ca40b52b6d795a57
atm0016 <atm0016@example.com>
2017-06-01 23:22:15 +0530

    Adjust style of index.php

--- index.php
+++ index.php
@@ -26,6 +26,22 @@
           <img src="img/product1.jpg" alt="">
           <p>sssssssssssssss</p>
         </div>
+        <div class="row midle-img-box">
+          <div class="span4 midle-img-box">
+            <h3>ssssss</h3>
+            <img src="img/product1.jpg" alt="">
+            <p>sssssssssssssss</p>
+          </div>
+          <div class="span4 midle-img-box">
+            <h3>ssssss</h3>
+            <img src="img/product1.jpg" alt="">
+            <p>sssssssssssssss</p>
+          </div>
+          <div class="span4 midle-img-box">
+            <h3>ssssss</h3>
+            <img src="img/product1.jpg" alt="">
+            <p>sssssssssssssss</p>
+          </div>
       </div>
     </div>
 
